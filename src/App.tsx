80759b9706232acfc5c6b51c84dd07d81b12748e
André Luiz <andrejunioracre@gmail.com>
2022-12-19 15:13:42 -0500

    removing StrictMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import { Pokemon } from './models/pokemon'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon`).then(response => {
+      setPokemons(response.data);
+      console.log(response.data.results)
+    })
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div>Hello world!</div>
   )
+
 }
 
 export default App
