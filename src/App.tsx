d52ee572dc1af4424849af4b0fdbaa5ff80f6bc7
André Luiz <andrejunioracre@gmail.com>
2022-12-20 10:20:28 -0500

    resolving problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
 import { useEffect, useState } from "react"
 import axios from 'axios';
-import { Pokemon } from './models/pokemon'
+import { Pokemon } from "./models/pokemon";
 
 function App() {
 
-  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  // useEffect(() => {
+  //   axios.get(`https://pokeapi.co/api/v2/pokemon`).then(response => {
+  //     response.data.results.forEach((pokemonLista: any) => {
+  //       axios.get(pokemonLista.url).then((pokemon: any) => {
+  //         setPokemons(pokemon.data);
+  //       })
+  //     });
+  //   })
+  // }, [])
+
+  // console.log(pokemons);
 
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon`).then(response => {
-      response.data.results.forEach((pokemonLista: any) => {
-        axios.get(pokemonLista.url).then((pokemon: any) => {
-          console.log({name: pokemon.data.name, type: [pokemon.data.types[0].type.name, pokemon.data.types[1]?.type.name]});
-        })
-      });
-    })
-  }, [])
+    getPokemons();
+  }, []);
 
+  const getPokemons = () => {
+    var endpoints = [];
+    for (var i = 1; i < 21; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    }
+    var response = axios.all(endpoints.map(async (endpoint) => (await axios.get(endpoint)).data)).then((res: Pokemon[]) => setPokemons(res));
+  }
+
+  console.log(pokemons);
   return (
-    <ul>
-      
-    </ul>
+    <div>
+      <ul>
+        {pokemons.map((pokemon) => {
+          return (
+            <li key={pokemon.name}>
+              <strong>{pokemon.name}</strong>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 
 }
